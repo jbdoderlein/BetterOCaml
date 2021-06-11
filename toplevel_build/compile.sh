6f0138fad6a22e3e23eee6af7a723dcac9683af7
jb <jbwolf@hotmail.fr>
2021-06-11 14:13:43 +0200

    output channel

--- toplevel_build/compile.sh
+++ toplevel_build/compile.sh
@@ -69,7 +69,7 @@ Build_Toplevel () {
     # Install dependencies
     echo "Installing dependencies ..."
     eval $(opam env)
-    opam install --yes tyxml ocp-indent higlo cohttp lwt tyxml reactiveData yojson graphics menhirLib cmdliner ppxlib react menhir dune num
+    opam install --yes tyxml ocp-indent higlo cohttp lwt tyxml reactiveData yojson graphics menhirLib cmdliner ppxlib react menhir dune
     eval $(opam env)
 
     # Build
